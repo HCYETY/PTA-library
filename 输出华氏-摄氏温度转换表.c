f4a1b75292a3ca0f117294c02837afc23954a395
HCYETY <70583417+HCYETY@users.noreply.github.com>
2020-10-10 19:53:24 +0800

    2020.10.10

---
+++ 输出华氏-摄氏温度转换表.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(void)
+{
+    int fahr, lower, upper;
+    double celsius;
+    scanf("%d %d", &lower, &upper);
+	
+    if(lower <= upper){
+	printf("fahr celsius\n");	
+        for(fahr = lower; fahr <= upper; fahr=fahr+2){
+	    celsius = 5.0* (fahr-32)/9.0;
+	    printf("%d%6.1f\n", fahr, celsius);
+	    }	
+    }else
+	printf("Invalid.");
+	
+    return 0;
+}
+
