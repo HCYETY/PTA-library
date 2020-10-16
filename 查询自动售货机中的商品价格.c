fd94f2820eb9bd7be41c0059342058c3c30feaaf
HCYETY <70583417+HCYETY@users.noreply.github.com>
2020-10-16 11:19:05 +0800

    2020.10.16

--- 查询自动售货机中的商品价格.c
+++ 查询自动售货机中的商品价格.c
@@ -2,26 +2,20 @@
 int main()
   {int x, y;
    float price;
-   for(x=1;x<=5;x++) {
    printf("[1] crisps\n");
    printf("[2] popcorn\n");
    printf("[3] chocolate\n");
    printf("[4] cola\n");
    printf("[0] exit\n");
-   printf("Enter y:");
+    for(x=1;x<=5;x++) {
    	scanf("%d",&y);
    	if(y==0) break;
    	switch(y){
-   	   case 1:
-           printf("price = 3.0");break;
-       case 2:
-           printf("price = 2.5");break;
-       case 3:
-           printf("price = 4.4");break;
-       case 4:
-           printf("price = 3.5");break;
-       default:
-           printf("price = 0.0");break;
+   	   case 1:price = 3.0;break;
+       case 2:price = 2.5;break;
+       case 3:price = 4.0;break;
+       case 4:price = 3.5;break;
+       default:price = 0.0;break;
 	   }
 	   printf("price = %.1f\n",price);
    }
