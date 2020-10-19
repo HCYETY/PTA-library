51236edce2b150e1eb0f8066332d7df08f1b2e57
HCYETY <70583417+HCYETY@users.noreply.github.com>
2020-10-19 22:09:22 +0800

    2020.10.19

--- 生成3的乘方表.c
+++ 生成3的乘方表.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main()
-  {int n, i;
-   double power;
+  {int n, i, power;
    scanf("%d",&n);
    for(i=0;i<=n;i++) {
     power = pow(3,i);
-	printf("pow(3,%d) = %.3f\n", i, power);
+	printf("pow(3,%d) = %d\n", i, power);
    }
   } 
