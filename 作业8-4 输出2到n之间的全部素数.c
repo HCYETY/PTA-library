02b7db937fc62d18e372831c2f144c15c126b195
HCYETY <70583417+HCYETY@users.noreply.github.com>
2020-11-09 21:29:26 +0800

    2020.11.09

--- 作业8-4 输出2到n之间的全部素数.c
+++ 作业8-4 输出2到n之间的全部素数.c
@@ -1,11 +1,47 @@
+/*
+本题要求输出2到n之间的全部素数，每行输出10个。素数就是只能被1和自身整除的正整数。
+注意：1不是素数，2是素数。
+
+输入格式:
+输入在一行中给出一个长整型范围内的整数。
+输出格式:
+输出素数，每个数占6位，每行输出10个。如果最后一行输出的素数个数不到10个，也需要换行。
+
+输入样例:10
+输出样例:     2     3     5     7
+*/ 
+
+/*
+编程思路：
+ 
+*/
+
+#include<stdio.h>
+int main()
+  {int n, count=0, j;
+   scanf("%d",&n);
+	for(int i=2; i<=n; i++) {
+		for(j=2; j<=i; j++) {
+			if(i%j==0)	break;
+    	}
+		if(j>=i) {  /*if语句不理解*/ 
+			printf("%6d",i);
+			count++;
+			if(count%10==0)
+				printf("\n");	
+			}	
+		}
+	return 0;
+  }
+
+/*2到100的素数*/
 #include <stdio.h>
 #include <math.h>   
 int main(void)
-  {int count, i, m, n;   
-   count = 0; 
+  {int count=0, m, n;    
    for(m = 2; m <= 100; m++) {
         n = sqrt(m);
-        for (i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
         	if(m % i == 0) break;
         	if(i > n){   		/* 如果m是素数 */
        			printf("%6d", m); 
