84cbbe99a2942a84d3a170348305b1211b88c9ab
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-04-07 14:25:47 +0800

    2021-04-07

---
+++ program title/树/二叉树的创建与遍历.cpp
@@ -0,0 +1,87 @@
+通过带空指针信息的先根序列（亦称先序序列）创建二叉树，并进行先根（先序）、中根（中序）、后根（后序）遍历。二叉树结点数据域值为不等于0的整数（可能是正数也可能是负数），空指针用0表示。
+
+输入格式:
+输入为一组用空格间隔的整数，表示带空指针信息的二叉树先根序列。其中空指针信息用0表示。二叉树结点个数不超过150000，高度不超过6000。输入数据保证二叉树各结点数据值互不相等。
+
+输出格式:
+输出为3行整数，每个整数后一个空格。第1行为该二叉树的先根序列，第2行为中根序列，第3行为后根序列。
+
+输入样例:
+1 5 8 0 0 0 6 0 0
+输出样例:
+1 5 8 6
+8 5 1 6
+8 5 6 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+	int number;
+	TreeNode* lchild, *rchild;
+
+	TreeNode():lchild(NULL), rchild(NULL){
+	}
+};
+
+//读入输入的数字串，构造二叉树
+TreeNode* create_tree();
+//传入树/子树tree，根结点root
+void preorder(TreeNode* tree);  //先序遍历
+void inorder(TreeNode* tree);   //中序遍历
+void postorder(TreeNode* tree);  //后序遍历
+
+int main()
+{
+	TreeNode* root = create_tree();
+
+	preorder(root);
+	cout<<endl;
+	inorder(root);
+	cout<<endl;
+	postorder(root);
+	cout<<endl;
+}
+
+TreeNode* create_tree()
+{
+	TreeNode* root = NULL;
+	int root_number;
+	if(cin>>root_number && root_number != 0)//更改 >变为！=  题目说：结点数据可能是正数也可能是负数
+	{
+		root = new TreeNode();
+		root->number = root_number;
+
+		root->lchild = create_tree();  //构建左子树
+		root->rchild = create_tree();  //构建右子树
+	}
+	return root;
+}
+void preorder(TreeNode* tree)
+{
+	if(tree)
+	{
+		cout<<tree->number<<" ";
+		preorder(tree->lchild);
+		preorder(tree->rchild);
+	}
+}
+void inorder(TreeNode* tree)
+{
+	if(tree)
+	{
+		inorder(tree->lchild);
+		cout<<tree->number<<" ";
+		inorder(tree->rchild);
+	}
+}
+void postorder(TreeNode* tree)
+{
+	if(tree)
+	{
+		postorder(tree->lchild);
+		postorder(tree->rchild);
+		cout<<tree->number<<" ";
+	}
+}
---
+++ program title/树/二叉树的遍历！（简单）.cpp
@@ -0,0 +1,122 @@
+这是一道简单的二叉树问题！
+我们将给出一颗二叉树，请你输出它的三种遍历，分别是先序遍历，中序遍历，后序遍历！
+
+输入格式:
+第一行给出一个正整数N（N<=100)，表示二叉树上的节点个数！
+接下来N行，每行包含三个整数，i，l，r，分别代表第i个节点的左右孩子！
+如果它的左/右孩子为空，则在对应位置给出-1！
+
+题目保证1是根节点！
+
+输出格式:
+第一行输出先序遍历，第二行输出中序遍历，第三行输出后序遍历！
+每行末尾无多余空格！
+
+输入样例:
+3
+1 2 3
+2 -1 -1
+3 -1 -1
+输出样例:
+1 2 3
+2 1 3
+2 3 1
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int NodeId;
+struct TreeNode
+{
+	int number;
+	NodeId left, right;
+	
+	TreeNode():left(-1), right(-1){
+	}
+};
+
+int sign = 0;
+//打印结点编号。 
+void print_node(NodeId number)
+{
+	if (sign == 0)
+		cout<<number;
+	else
+		cout<<" "<<number;
+	sign += 1;
+}
+
+//传入树/子树tree，根结点root
+void preorder(TreeNode* tree, NodeId root);  //先序遍历
+void inorder(TreeNode* tree, NodeId root);   //中序遍历
+void postorder(TreeNode* tree, NodeId root);  //后序遍历
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	TreeNode* tree = new TreeNode[n+1];  //分配n个结点的存储空间 
+	for(int i=0; i<n; i++)
+	{
+		int father, left, right;
+		cin>>father>>left>>right;
+  		tree[father].number = father;
+		tree[father].left = left;  //串接左孩子
+		tree[father].right = right; //串接右孩子
+	}
+	NodeId root = 1;  //1号结点是根节点 
+	//先序遍历
+	sign = 0;
+	preorder(tree, 1); 
+	cout<<endl;
+	//中序遍历 
+	sign = 0;
+	inorder(tree, 1); 
+	cout<<endl;
+	//后序遍历
+	sign = 0;
+	postorder(tree, 1); 
+	cout<<endl;	
+}
+
+//先序遍历
+//传入树/子树tree，根结点root
+void preorder(TreeNode* tree, NodeId root)
+{
+	if (root == -1)
+		return ;
+		
+	print_node(tree[root].number);
+	preorder(tree, tree[root].left);
+	preorder(tree, tree[root].right);
+	return ;
+} 
+
+
+//中序遍历
+//传入树/子树tree，根结点root
+void inorder(TreeNode* tree, NodeId root)
+{
+	if (root == -1)
+		return ;
+		
+	inorder(tree, tree[root].left);
+	print_node(tree[root].number);
+	inorder(tree, tree[root].right);
+	return ;
+} 
+
+//后序遍历
+//传入树/子树tree，根结点root
+void postorder(TreeNode* tree, NodeId root)
+{
+	if (root == -1)
+		return ;
+		
+	postorder(tree, tree[root].left);
+	postorder(tree, tree[root].right);
+	print_node(tree[root].number);
+	return ;
+} 
